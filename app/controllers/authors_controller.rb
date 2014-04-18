9925e4cf7e9649c192e8c3a3648e07a5d17f8abe
cheri allen <cherimarie@gmail.com>
2014-04-18 11:58:42 -0700

    Devise up and running

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,5 @@
 class AuthorsController < ApplicationController
+  before_filter :authenticate_user!, except: [:index, :show]
 
   def index
     @authors = Author.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 LibraryCollection::Application.routes.draw do
-  devise_for :users
+  devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
   resources :books
   resources :genres
   resources :authors
