fcab54c3c55a982c8a9ef75b2a84e459cc8fd6bd
cheri allen <cherimarie@gmail.com>
2014-04-23 14:49:22 -0700

    Add a few basic tests

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe StaticPagesController do 
+  
+
+
+  it "should have a current_user" do
+    login_user
+    visit root_path 
+    page { should have_content(user.email) }
+  end
+
+  it "should show admin links to admins" do
+    login_admin
+    visit root_path 
+    page { should have_selector('a', text: 'Admin Dashboard') }
+  end
+
+  it "should not show admin links to non-admins" do
+    login_user
+    visit root_path 
+    page { should_not have_selector('a', text: 'Admin Dashboard') }
+  end
+
+end 
