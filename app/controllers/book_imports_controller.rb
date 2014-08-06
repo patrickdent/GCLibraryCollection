3a1fb23db0e8d3dbf0e4c2c538bfe4c1aced86f1
forplayplay <psdent@gmail.com>
2014-08-06 13:49:22 -0700

    exceptions and handling forbad fyle types/formatting
    Conflicts:
    	app/models/book_import.rb

--- app/controllers/book_imports_controller.rb
+++ app/controllers/book_imports_controller.rb
@@ -1,6 +1,8 @@
 class BookImportsController < ApplicationController
   include BookImportsHelper
   require 'csv'
+  rescue_from BookImport::InvalidFileError, with: :invalid_file
+  rescue_from CSV::MalformedCSVError, with: :invalid_file
 
   def new
     @book_import = BookImport.new
@@ -10,13 +12,18 @@ class BookImportsController < ApplicationController
      if BookImport.import_requirements?(params)
       @book_import = BookImport.new(book_imports_params)
       if @book_import.save
-        flash[:notice] = "Import Succssful"
-      else
-        render "static_pages/home"
+        flash[:notice] = "Import Successful"
+      # else
+      #   render "static_pages/home"
       end
     else
       flash[:notice] = "Please select a file."
       render "static_pages/home"
     end
+  end
+
+  def invalid_file
+    flash[:notice] = "Import Unsuccessful: Please Ensure That The File Is Formatted Correctly."
+    redirect_to :back
   end
 end
