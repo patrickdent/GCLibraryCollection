c23179b8416c4633e65b7ca92372d56646325cc1
cheri allen <cherimarie@gmail.com>
2014-08-18 13:21:57 -0700

    Add helpful downcasing so PG can search more effectively

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,10 +6,8 @@ class Book < ActiveRecord::Base
   validates :title, presence: true 
 
   def self.search(search)
-    find(:all, conditions: ['title LIKE ?', "%#{search}%"] )
+    find(:all, conditions: ['lower(title) LIKE lower(?)', "%#{search}%"] )
   end
-  # def initialize( aTitle )
-  #   @title = aTitle
-  # end
+
 
 end
