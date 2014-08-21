229df17c95f4f8b16aa8917f4edc23f9b36eee60
cheri allen <cherimarie@gmail.com>
2014-08-20 17:54:30 -0700

    Ability to add new genres on genre management page

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -6,4 +6,25 @@ class GenresController < ApplicationController
   def show
     @genre = Genre.find(params[:id])
   end
+
+  def new 
+    @genre = Genre.new
+    @genres = Genre.all
+  end 
+
+  def create 
+    @genre = Genre.new(genre_params)
+    if @genre.save 
+      flash[:notice] = "Genre added"
+      redirect_to new_genre_path
+    else
+      flash[:alert] = "Genre failed to save"
+      redirect_to new_genre_path
+    end 
+  end 
+
+  private
+  def genre_params
+    params.require(:genre).permit(:name, :id)
+  end 
 end
