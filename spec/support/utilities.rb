26ff382a5771fa5c5b513f066a6a34827b5a79af
forplayplay <psdent@gmail.com>
2014-09-18 10:25:38 -0700

    login as test admin functional

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,8 +1,9 @@
 
 def admin_login
+  admin = FactoryGirl.create(:admin, password: "password", email: "email@email.com")
   visit root_path
   click_on('Login')
-  fill_in("Email", :with => 'admin@example.com')
+  fill_in("Email", :with => admin.email)
   fill_in("Password", :with => 'password')
   click_on("Sign in")
 end
