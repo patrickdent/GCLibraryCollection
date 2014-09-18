03e3f9cbecb5b6e7fbf3d6bb8ecd777fb3f20a7f
cheri allen <cherimarie@gmail.com>
2014-09-18 11:29:51 -0700

    Explicit return on empty search params

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,8 +2,9 @@ class SearchController < ApplicationController
   
   def search
     if params[:search] == ""
-      redirect_to root_path
       flash[:notice] = "Please enter a search term."
+      redirect_to root_path
+      return 
     else
       @authors = Author.search(params[:search])
       @books = Book.search(params[:search])
