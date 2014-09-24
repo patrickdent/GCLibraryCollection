f2d44a3606f8b2e8fb20e950922d741cc278bc63
forplayplay <psdent@gmail.com>
2014-09-24 12:12:35 -0700

    scraping in progress

---
+++ app/models/search.rb
@@ -0,0 +1,19 @@
+class Search
+  require 'net/http'
+
+  def scrape
+    # get https://www.googleapis.com/books/v1/volumes?q=isbn:9780547539638&key=ENV['google_api_key']
+    # begin
+      url = URI.parse('https://www.googleapis.com/books/v1/volumes?q=isbn:9780547539638&key=AIzaSyA7GlnCX6M6CHniJmHO7BgAwdIu8ROYIQU')
+      req = Net::HTTP::Get.new(url.to_s)
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
+      response = http.request(req)
+      puts response.body
+    # rescue URI::InvalidURIError
+    #   host = url.match(".+\:\/\/([^\/]+)")[1]
+    #   path = url.partition(host)[2] || "/"
+    #   Net::HTTP.get host, path
+    # end
+  end
+end
