36f5e4517927fc368b9ecef46169ecfe715b19a2
cheri allen <cherimarie@gmail.com>
2014-09-25 15:36:40 -0700

    Pull isbn into variable

--- app/models/search.rb
+++ app/models/search.rb
@@ -4,7 +4,8 @@ class Search
   def scrape
     # get https://www.googleapis.com/books/v1/volumes?q=isbn:9780547539638&key=ENV['google_api_key']
     # begin
-      url = URI.parse('https://www.googleapis.com/books/v1/volumes?q=isbn:9780547539638&key=')
+      isbn = '0374399387'
+      url = URI.parse("https://www.googleapis.com/books/v1/volumes?q=isbn:#{isbn}&key=")
       req = Net::HTTP::Get.new(url.to_s + ENV['google_api_key'])
       http = Net::HTTP.new(url.host, url.port)
       http.use_ssl = true
