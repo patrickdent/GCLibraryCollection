2d881cd069d1c55828f1738aab6e49a0d2d9bbdb
forplayplay <psdent@gmail.com>
2014-10-02 10:11:54 -0700

    cleaner parser

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,37 +1,37 @@
 class Search
   require 'net/http'
 
-  def scrape
-      isbn = '0374399387'
+  def scrape(isbn)
+      # isbn = '0374399387'
       url = URI.parse("https://www.googleapis.com/books/v1/volumes?q=isbn:#{isbn}&key=")
       req = Net::HTTP::Get.new(url.to_s + ENV['google_api_key'])
       http = Net::HTTP.new(url.host, url.port)
       http.use_ssl = true
       response = http.request(req)
       body = response.body
-      lines = Array.new
-      body.each_line do |l|
-        if ((l.include? "title") || (l.include? "description")) then
-          l.strip!
-          l.delete! "\""
-          lines << l
-        end
-      end
-      hash = Hash.new
-      lines.each do |l|
-        pair = l.split ":"
-        pair[1].strip!
-        pair[1].slice!(-1)
-        hash[pair[0].to_sym] = pair[1]
-      end
+      temp_hash = JSON.parse(body)
+      book_hash = temp_hash["items"].first["volumeInfo"]
+      authors = temp_hash["items"].first["volumeInfo"]["authors"]
+      # book_info = Array.new
+      # body.each_line do |l|
+      #   #can also do publisher, publishedDate, pageCount, 
+      #   if ((l.include? "title") || (l.include? "description") ||
+      #       (l.include? "publisher") || (l.include? "publishedDate") ||
+      #       (l.include? "pageCount")) then
+      #     l.strip!
+      #     l.delete! "\""
+      #     book_info << l
+      #   end
+      # end
+      # book_hash = Hash.new
+      # book_info.each do |l|
+      #   pair = l.split ":"
+      #   pair[1].strip!
+      #   pair[1].slice!(-1)
+      #   book_hash[pair[0].to_sym] = pair[1]
+      # end
 
-      # body.delete! "\""
-      # body.delete! "{"
-      # body.delete! "}"
-      # puts body
-      # return body
-      # puts response.body
-      # return response
-      return hash
+      puts book_hash
+      puts authors
   end
 end
