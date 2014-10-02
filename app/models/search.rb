6f24991cf01bab401185ac1fcac88faada072c52
forplayplay <psdent@gmail.com>
2014-10-01 21:22:34 -0700

    very ugly parser for book info

--- app/models/search.rb
+++ app/models/search.rb
@@ -2,19 +2,36 @@ class Search
   require 'net/http'
 
   def scrape
-    # get https://www.googleapis.com/books/v1/volumes?q=isbn:9780547539638&key=ENV['google_api_key']
-    # begin
       isbn = '0374399387'
       url = URI.parse("https://www.googleapis.com/books/v1/volumes?q=isbn:#{isbn}&key=")
       req = Net::HTTP::Get.new(url.to_s + ENV['google_api_key'])
       http = Net::HTTP.new(url.host, url.port)
       http.use_ssl = true
       response = http.request(req)
-      puts response.body
-    # rescue URI::InvalidURIError
-    #   host = url.match(".+\:\/\/([^\/]+)")[1]
-    #   path = url.partition(host)[2] || "/"
-    #   Net::HTTP.get host, path
-    # end
+      body = response.body
+      lines = Array.new
+      body.each_line do |l|
+        if ((l.include? "title") || (l.include? "description")) then
+          l.strip!
+          l.delete! "\""
+          lines << l
+        end
+      end
+      hash = Hash.new
+      lines.each do |l|
+        pair = l.split ":"
+        pair[1].strip!
+        pair[1].slice!(-1)
+        hash[pair[0].to_sym] = pair[1]
+      end
+
+      # body.delete! "\""
+      # body.delete! "{"
+      # body.delete! "}"
+      # puts body
+      # return body
+      # puts response.body
+      # return response
+      return hash
   end
 end
