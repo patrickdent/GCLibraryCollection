e21a613d8d391125e792cc00d07137ea1b1220f6
forplayplay <psdent@gmail.com>
2014-10-30 15:25:26 -0700

    prelim success on labels

--- app/models/document_generator.rb
+++ app/models/document_generator.rb
@@ -1,23 +1,34 @@
 class DocumentGenerator
   include Prawn::View
+  require "prawn/measurement_extensions"
 
   def initialize(format)
     @dimentions = {}
+    @format = format
     set_format(format)
   end
+
+  def generate_document(info)
+    case @format
+    when :label
+      make_labels(info)
+    else
+      puts "FAILURE IN generate_document"
+    end
+  end
+
+  private
 
   def dimentions
     @dimentions
   end
-
-  private
 
   def set_format(format)
     case format
     when :label
       set_label_format
     else
-      return
+      puts "ERROR IN set_format"
     end
   end
 
@@ -29,9 +40,58 @@ class DocumentGenerator
     @dimentions[:bottom_margin] = 0.47
     @dimentions[:left_margin] = 0.39
     @dimentions[:right_margin] = 0.31
-    @dimentions[:padding] = 0.3
+    
+    @dimentions[:gutter] = 0.3
 
     @dimentions[:rows] = 15
     @dimentions[:columns] = 4
+  end
+
+  def make_labels(info)
+    document = Prawn::Document.new({page_size: "LETTER",
+                                    margin: [@dimentions[:top_margin].in, 
+                                             @dimentions[:right_margin].in,
+                                             @dimentions[:bottom_margin].in,
+                                             @dimentions[:left_margin].in]})  
+
+    #to count labels to know when to go to a new line
+    @label_count = 0
+    #the x,y position for the upper-left corner
+    #of the label
+    @x = 0
+    @y = document.cursor
+
+    info.each do |i|
+      new_label(i, @x, @y, document)
+      @label_count += 1
+      #change 4 to @dimentions[:columns]
+      if @label_count % 60 == 0 then
+        document.start_new_page
+        @y = cursor
+        @x = 0
+      elsif @label_count % 4 == 0 then 
+        @y -= 0.66.in
+        @x = 0
+      else
+        #change to @dim...:width :gutter
+        @x += ( 1.75.in + 0.3.in )   
+      end
+    end      
+ 
+    document.render_file "test.pdf"
+  end
+
+  def new_label(info, x, y, document)
+    float do
+      document.bounding_box([x, y], width: 1.75.in, height: 0.66.in) do
+        #use padding instead of a new line here if you can
+        document.text_box( "#{info.title}
+                            #{info.authors.first.name}
+                            #{info.genre.name}
+                            #{info.isbn}
+                            ", 
+                            overflow: :shrink_to_fit )
+      end
+    end
   end
 end
--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -17,10 +17,10 @@ authors = Author.create([{ name: 'Chairman Meow' },
                          { name: 'B. Author' }, 
                          { name: 'Boots McNally'}])
 
-books = Book.create([{ title: 'Kittles and Bits', genre: genres[0] }, 
-                     { title: 'I Am Who Am I?', genre: genres[1] },
-                     { title: 'The Sound and the Furry', genre: genres[0] },
-                     { title: 'Meowtains', genre: genres[2] }])
+books = Book.create([{ title: 'Kittles and Bits', genre: genres[0], isbn: 1234567890 }, 
+                     { title: 'I Am Who Am I?', genre: genres[1], isbn: 1123456789 },
+                     { title: 'The Sound and the Furry', genre: genres[0], isbn: 1112345678 },
+                     { title: 'Meowtains', genre: genres[2], isbn: 1111234567 }])
 
 keywords = Keyword.create([{ name: 'good' },
                            { name: 'bad' },
