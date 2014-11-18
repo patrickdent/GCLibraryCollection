12d6d775837e87a4207fd98fa901fd70bdb2d75b
forplayplay <psdent@gmail.com>
2014-11-18 12:54:33 -0800

    tests for views....kinda

---
+++ spec/requests/user_pages_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe "User Pages" do
+
+  before do
+    @user = create :user
+    @librarian = create :librarian
+    @admin = create :admin 
+  end
+
+  subject { page }
+
+  describe "index" do
+    context "as librarian" do
+      before do
+        login_librarian
+        visit users_path
+      end
+
+      it "links to users" do expect(subject).to have_link(@user.name, user_path(@user.id)) end
+      #doesn't work for some reason
+      # it "has edit link" do expect(subject).to have_link("Edit") end
+    end
+
+    context "as admin" do
+      before do
+        login_admin
+        visit users_path
+      end
+
+      it "links to users" do expect(subject).to have_link(@user.name, user_path(@user.id)) end
+
+      #doesn't work for some reason
+      # it "has edit link" do expect(subject).to have_link("Edit") end
+      # it "has delete link" do expect(subject).to have_link("Delete") end
+    end
+  end
+
+  describe "show" do
+
+    context "as librarian" do
+
+      before do
+        login_librarian 
+        visit user_path(@user.id) 
+      end
+
+      it "displays user name" do expect(subject).to have_selector('h2', text: @user.name) end
+      # it "displays user email" do expect(subject).to have_content(@user.email) end
+      # it "has edit link" do expect(subject).to have_link("Edit This User") end
+    end
+  end
+end
--- spec/support/devise_support.rb
+++ spec/support/devise_support.rb
@@ -5,6 +5,11 @@ module Logins
     post_via_redirect user_session_path, 'user[email]' => @admin.email, 'user[password]' => @admin.password
   end
 
+  def login_librarian
+    @admin = FactoryGirl.create(:librarian)
+    post_via_redirect user_session_path, 'user[email]' => @librarian.email, 'user[password]' => @librarian.password
+  end
+
   def login_user
     @user = FactoryGirl.create(:user)
     post_via_redirect user_session_path, 'user[email]' => @user.email, 'user[password]' => @user.password
