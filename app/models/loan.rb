bfb42f3eab1cd34a10f8d513fae585bc672b31a6
forplayplay <psdent@gmail.com>
2014-11-24 19:38:37 -0800

    model methods and tests

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -2,17 +2,31 @@ class Loan < ActiveRecord::Base
   belongs_to :book 
   belongs_to :user
 
-  after_create :set_start_date
+  after_create :set_lending_info
+
+  DURATION = 30
 
   def self.loan_book(book, user)
     book.users << user
     return book.loans.where(user: user, returned_date: nil).first
   end
 
+  def return_book
+    update_attribute(:returned_date, Time.now.to_date)
+    return returned_date
+  end
+
+  def renew_loan
+    update_attribute(:due_date, (due_date + DURATION))
+    update_attribute(:renewal_count, ( renewal_count + 1 ) )
+    return due_date
+  end
+
   private
 
-  def set_start_date
-    update_attribute(:start_date, Time.now) unless start_date
+  def set_lending_info
+    update_attribute(:start_date, Time.now.to_date) unless start_date
+    update_attribute(:due_date, DURATION.days.from_now) unless due_date 
+    update_attribute(:renewal_count, 0) unless renewal_count
   end
- 
 end
--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -8,8 +8,35 @@ describe Loan do
   it "has user" do expect(loan.user).to eq(user) end
   it "has book" do expect(loan.book).to eq(book) end
   it "has start date" do expect(loan).to respond_to(:start_date) end
-  it "has due date" do expect(loan).to respond_to(:due_date) end
+  it "has due date" do expect(loan.due_date).to eq(Loan::DURATION.days.from_now.to_date) end
   it "has returned date" do expect(loan).to respond_to(:returned_date) end
   it "has renewal count" do expect(loan).to respond_to(:renewal_count) end
 
+  describe "returning books" do
+    before { @loan = Loan.loan_book( (create :book), (create :user) ) }
+    
+    it "sets a return date" do
+      date = @loan.return_book
+      expect(date).to_not eq(nil) 
+      expect(date).to eq(@loan.returned_date) 
+    end
+  end
+
+  describe "renewing books" do
+    before { @loan = Loan.loan_book( (create :book), (create :user) ) }
+    
+    it "sets a new return date" do
+      old_date = @loan.due_date
+      date = @loan.renew_loan
+      expect(date).to_not eq(old_date) 
+      expect(date).to eq(@loan.due_date) 
+    end
+
+    it "increments renewal count" do 
+      old_count = @loan.renewal_count
+      @loan.renew_loan
+      count = @loan.renewal_count
+      expect(count).to eq(old_count + 1) 
+    end
+  end
 end
