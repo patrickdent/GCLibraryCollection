1bdc05bab290b8466f85f6e731a7c9945c5e6e50
forplayplay <psdent@gmail.com>
2014-11-28 17:20:32 -0800

    loan views incl tests: still need JS for renewal/returns

--- spec/requests/loan_pages_spec.rb
+++ spec/requests/loan_pages_spec.rb
@@ -51,4 +51,32 @@ describe "Loan Pages" do
       it "displays return date" do expect(page).to have_content(@loan.returned_date) end
     end
   end
+
+  describe 'form' do
+    context 'on user#show' do
+      before do
+        librarian_login 
+        @loan = create(:loan, user_id: user.id, book_id: book.id) 
+        visit user_path(user.id)
+      end
+
+      it "shows loan title" do expect(page).to have_link(book.title) end
+      it "displays start date" do expect(page).to have_content(@loan.start_date) end
+      it "displays due date" do expect(page).to have_content(@loan.due_date) end
+      it "displays renewals" do expect(page).to have_content(@loan.renewal_count) end
+    end
+
+    context 'on book#show' do
+      before do
+        librarian_login 
+        @loan = create(:loan, user_id: user.id, book_id: book.id) 
+        visit book_path(book.id)
+      end
+
+      it "shows loan title" do expect(page).to have_link(user.name) end
+      it "displays start date" do expect(page).to have_content(@loan.start_date) end
+      it "displays due date" do expect(page).to have_content(@loan.due_date) end
+      it "displays renewals" do expect(page).to have_content(@loan.renewal_count) end
+    end
+  end  
 end
