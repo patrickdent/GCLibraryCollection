60710c0585fe2619a83f84c51fefadd59d1edfda
forplayplay <psdent@gmail.com>
2015-01-09 13:52:57 -0800

    fixed lingering issues with switch to pref_name

--- app/models/user_upload.rb
+++ app/models/user_upload.rb
@@ -49,7 +49,7 @@ class UserUpload < ActiveRecord::Base
     else
       user = User.create( name: user_data["name"],
                     address: user_data["address"],
-                    username: user_data["name"].join,
+                    username: user_data["name"].delete(" "),
                     phone: user_data["phone"],
                     password: "password" )
     end
