4c9d6da1f1095fb3d9b1dd89a820dfb3151849c8
cheri allen <cherimarie@gmail.com>
2015-02-17 10:30:02 -0800

    Setup for gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,12 +80,13 @@ LibraryCollection::Application.configure do
 
   #config.action_mailer.default_url_options = { host: 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
+  # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'example.com',
-    user_name:            '<username>',
-    password:             '<password>',
-    authentication:       'plain',
-    enable_starttls_auto: true  }
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_password'],
+   :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
 end
