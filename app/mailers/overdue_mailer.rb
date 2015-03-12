7d5e7727bc998c36bdd1989c9e9cd0f7b64c184d
cheri allen <cherimarie@gmail.com>
2015-03-12 12:20:25 -0700

    Simplify accessor

--- app/mailers/overdue_mailer.rb
+++ app/mailers/overdue_mailer.rb
@@ -3,16 +3,12 @@ class OverdueMailer < ActionMailer::Base
 
   @last_sent = Date.new(2000,1,1)
 
+  class << self
+    attr_accessor :last_sent
+  end
+
   def overdue_reminder(user)
     @user = user
     mail(to: @user.email, subject: 'Overdue Reminder')
-  end
-
-  def self.last_sent=(date)
-    @last_sent = date
-  end
-
-  def self.last_sent
-    @last_sent
   end
 end
