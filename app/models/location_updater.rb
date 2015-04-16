bab16c2b5a80bfdd07cdab06164705501d26996e
forplayplay <psdent@gmail.com>
2015-04-16 12:32:26 -0700

    functional for both storage and missing, tracks failures

--- app/models/location_updater.rb
+++ app/models/location_updater.rb
@@ -1,16 +1,29 @@
 class LocationUpdater
   require 'csv'
+  
+  attr_accessor :failures
 
-  def self.update_locations
-    print "Name of file:"
-    file = gets.strip
-    data = LocationUpdater::open_csv(file)
-
-    data.each { |book| LocationUpdater::update_book(book) }
-    # puts data
+  def initialize
+    @failures = Array.new
   end
 
-  def self.open_csv(file)
+  def update_locations
+    print "Path to storage file:"
+    storage_file = gets.strip
+    print "Path to storage file:"
+    missing_file = gets.strip
+
+    storage_data = open_csv(storage_file)
+    missing_data = open_csv(missing_file)
+
+    storage_data.each { |book| update_storage_book(book) }
+    missing_data.each { |book| update_missing_book(book) }
+
+    puts "SOME FILES DIDN'T UPDATE CORRECTLY!!" unless @failures.empty?
+    puts "Everything went swimmingly" if @failures.empty?
+  end
+
+  def open_csv(file)
     unassigned_data = CSV.read(file, encoding: "bom|utf-8")
 
     unassigned_data[0].each { |col| col = col.downcase! }
@@ -19,9 +32,20 @@ class LocationUpdater
     return data
   end
 
-  def self.update_book(book_info)
-    book = Book.find_by_title_and_isbn(book_info["title"], book_info["isbn"])
-    book.update_attributes(in_storage: true, location: book_info["owner"])
-    puts book.location
+  def update_storage_book(book_info)
+    if book = Book.find_by_title_and_isbn(book_info["title"], book_info["isbn"])
+      book.update_attributes(in_storage: true, location: book_info["owner"])
+    else
+      failures << [book_info["title"], book_info["isbn"], book_info["location"]]
+    end
+  end
+
+  def update_missing_book(book_info)
+    if book = Book.find_by_title_and_isbn(book_info["title"], book_info["isbn"])
+      book.update_attributes(missing: true, available: false)
+    else
+      failures << [book_info["title"], book_info["isbn"], book_info["location"]]
+    end
   end  
+  
 end
