2af74c32fe632a2bbdd664ba8e87e0f1b9d6d43a
forplayplay <psdent@gmail.com>
2015-04-17 10:06:00 -0700

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413164911) do
+ActiveRecord::Schema.define(version: 20150415193538) do
 
   create_table "authors", force: true do |t|
     t.string   "name"
