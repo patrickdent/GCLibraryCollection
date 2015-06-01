892f9dd9f2979a8519ca3a2010a350ec9acf065d
forplayplay <psdent@gmail.com>
2015-06-01 11:25:59 -0700

    first author sort functional

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,10 +10,15 @@ class BooksController < ApplicationController
 
   def index
     case params["sort"]
-    when "name"
-      @books = Book.joins(:genre).includes(:authors).order(sort_column + " " + sort_direction).paginate(:page => params[:page], :per_page => 50)
+    when "cat_name"
+      @books = Book.joins(:genre).includes(:authors)
+      .order(sort_column("name") + " " + sort_direction).paginate(:page => params[:page], :per_page => 50)
+    when "auth_name"
+      @books = Book.joins(:authors).includes(:genre)
+      .order(sort_column("name") + " " + sort_direction).paginate(:page => params[:page], :per_page => 50)
     else
-      @books = Book.includes(:authors, :genre).order(sort_column + " " + sort_direction).paginate(:page => params[:page], :per_page => 50)
+      @books = Book.includes(:authors, :genre)
+      .order(sort_column + " " + sort_direction).paginate(:page => params[:page], :per_page => 50)
     end
   end
 
@@ -88,9 +93,9 @@ class BooksController < ApplicationController
 
   def show_list
     case params["sort"]
-    when "name"
+    when "cat_name"
       @books = Book.joins(:genre).includes(:authors).where(id: session[:selected_books])
-      .order(sort_column + " " + sort_direction).paginate(:page => params[:page], :per_page => 50)
+      .order(sort_column("name") + " " + sort_direction).paginate(:page => params[:page], :per_page => 50)
     else
       @books = Book.includes(:authors, :genre).where(id: session[:selected_books])
       .order(sort_column + " " + sort_direction).paginate(:page => params[:page], :per_page => 50)
@@ -112,8 +117,12 @@ class BooksController < ApplicationController
     @book = Book.find_by(id: params[:id])
   end
 
-  def sort_column(default = "title")
-    params[:sort] ? params[:sort] : default
+  def sort_column(column_name = nil)
+    unless column_name
+      params[:sort] ? params[:sort] : "title"
+    else
+      column_name
+    end
   end
 
   def sort_direction(default = "asc")
