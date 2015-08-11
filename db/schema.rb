459b128278f56efdca67fca89829c3ecf3d2eacb
NIRD DevOps <devops@nird.us>
2015-08-10 19:27:24 -0700

    Schema

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,17 @@
 
 ActiveRecord::Schema.define(version: 20150712192651) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+  enable_extension "hstore"
+  enable_extension "pg_stat_statements"
+
   create_table "authors", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "sort_by"
   end
-
-  add_index "authors", ["name"], name: "index_authors_on_name"
 
   create_table "book_authors", force: true do |t|
     t.integer  "book_id"
@@ -31,8 +34,8 @@ ActiveRecord::Schema.define(version: 20150712192651) do
     t.boolean  "primary",         default: false
   end
 
-  add_index "book_authors", ["author_id"], name: "index_book_authors_on_author_id"
-  add_index "book_authors", ["book_id"], name: "index_book_authors_on_book_id"
+  add_index "book_authors", ["author_id"], name: "index_book_authors_on_author_id", using: :btree
+  add_index "book_authors", ["book_id"], name: "index_book_authors_on_book_id", using: :btree
 
   create_table "book_keywords", force: true do |t|
     t.integer  "book_id"
@@ -41,8 +44,8 @@ ActiveRecord::Schema.define(version: 20150712192651) do
     t.datetime "updated_at"
   end
 
-  add_index "book_keywords", ["book_id"], name: "index_book_keywords_on_book_id"
-  add_index "book_keywords", ["keyword_id"], name: "index_book_keywords_on_keyword_id"
+  add_index "book_keywords", ["book_id"], name: "index_book_keywords_on_book_id", using: :btree
+  add_index "book_keywords", ["keyword_id"], name: "index_book_keywords_on_keyword_id", using: :btree
 
   create_table "book_uploads", force: true do |t|
     t.datetime "created_at"
@@ -71,10 +74,10 @@ ActiveRecord::Schema.define(version: 20150712192651) do
     t.boolean  "keep_multiple"
   end
 
-  add_index "books", ["genre_id"], name: "index_books_on_genre_id"
-  add_index "books", ["isbn"], name: "index_books_on_isbn"
-  add_index "books", ["location"], name: "index_books_on_location"
-  add_index "books", ["title"], name: "index_books_on_title"
+  add_index "books", ["genre_id"], name: "index_books_on_genre_id", using: :btree
+  add_index "books", ["isbn"], name: "index_books_on_isbn", using: :btree
+  add_index "books", ["location"], name: "index_books_on_location", using: :btree
+  add_index "books", ["title"], name: "index_books_on_title", using: :btree
 
   create_table "contributions", force: true do |t|
     t.string   "name"
@@ -89,7 +92,7 @@ ActiveRecord::Schema.define(version: 20150712192651) do
     t.string   "abbreviation", null: false
   end
 
-  add_index "genres", ["name"], name: "index_genres_on_name"
+  add_index "genres", ["name"], name: "index_genres_on_name", using: :btree
 
   create_table "keywords", force: true do |t|
     t.string   "name"
@@ -97,7 +100,7 @@ ActiveRecord::Schema.define(version: 20150712192651) do
     t.datetime "updated_at"
   end
 
-  add_index "keywords", ["name"], name: "index_keywords_on_name"
+  add_index "keywords", ["name"], name: "index_keywords_on_name", using: :btree
 
   create_table "loans", force: true do |t|
     t.integer  "user_id"
@@ -110,8 +113,8 @@ ActiveRecord::Schema.define(version: 20150712192651) do
     t.datetime "updated_at"
   end
 
-  add_index "loans", ["book_id"], name: "index_loans_on_book_id"
-  add_index "loans", ["user_id"], name: "index_loans_on_user_id"
+  add_index "loans", ["book_id"], name: "index_loans_on_book_id", using: :btree
+  add_index "loans", ["user_id"], name: "index_loans_on_user_id", using: :btree
 
   create_table "roles", force: true do |t|
     t.string   "name"
@@ -121,8 +124,8 @@ ActiveRecord::Schema.define(version: 20150712192651) do
     t.datetime "updated_at"
   end
 
-  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id"
-  add_index "roles", ["name"], name: "index_roles_on_name"
+  add_index "roles", ["name", "resource_type", "resource_id"], name: "index_roles_on_name_and_resource_type_and_resource_id", using: :btree
+  add_index "roles", ["name"], name: "index_roles_on_name", using: :btree
 
   create_table "user_uploads", force: true do |t|
     t.string   "file"
@@ -157,17 +160,17 @@ ActiveRecord::Schema.define(version: 20150712192651) do
     t.boolean  "deactivated",            default: false
   end
 
-  add_index "users", ["email"], name: "index_users_on_email"
-  add_index "users", ["name"], name: "index_users_on_name"
-  add_index "users", ["preferred_first_name"], name: "index_users_on_preferred_first_name"
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["username"], name: "index_users_on_username"
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["name"], name: "index_users_on_name", using: :btree
+  add_index "users", ["preferred_first_name"], name: "index_users_on_preferred_first_name", using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", using: :btree
 
   create_table "users_roles", id: false, force: true do |t|
     t.integer "user_id"
     t.integer "role_id"
   end
 
-  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
+  add_index "users_roles", ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id", using: :btree
 
 end
